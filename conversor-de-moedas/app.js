53a31a1b14f857f8f699ebda9c5ded4d4f1b2d87
v-rapha <vs.raphael19@gmail.com>
2021-04-25 00:02:19 -0300

    'conversor-de-moedas' code after refactoring pt.2

--- conversor-de-moedas/app.js
+++ conversor-de-moedas/app.js
@@ -33,8 +33,10 @@ const currencyOneEl = document.querySelector('[data-js="currency-one"]');
 const currencyTwoEl = document.querySelector('[data-js="currency-two"]');
 const currenciesEl = document.querySelector('[data-js="currencies-container"]');
 const convertedValueEl = document.querySelector('[data-js="converted-value"]');
-const valuePrecisionEl = document.querySelector('[data-js="conversion-precision"]');
-const timesCurrencyOneEl = document.querySelector('[data-js="currency-one-times"]');
+const valuePrecisionEl = document
+  .querySelector('[data-js="conversion-precision"]');
+const timesCurrencyOneEl = document
+  .querySelector('[data-js="currency-one-times"]');
 
 const showAlert = err => {
   const div = document.createElement('div');
@@ -47,9 +49,8 @@ const showAlert = err => {
   button.setAttribute('type', 'button');
   button.setAttribute('aria-label', 'Close');
 
-  button.addEventListener('click', () => {
-    div.remove();
-  });
+  const removeAlert = () => div.remove();
+  button.addEventListener('click', removeAlert);
 
   div.appendChild(button);
   currenciesEl.insertAdjacentElement('afterend', div);
@@ -62,7 +63,10 @@ const state = (() => {
     getExchangeRate: () => exchangeRate,
     setExchangeRate: newExchangeRate => {
       if (!newExchangeRate.conversion_rates) {
-        showAlert({ message: 'O objeto precisa ter uma propriedade conversion_rates' });
+        showAlert({ 
+          message: 'O objeto precisa ter uma propriedade conversion_rates' 
+        });
+        
         return;
       }
 
@@ -72,7 +76,9 @@ const state = (() => {
   }
 })();
 
-const getUrl = currency => `https://v6.exchangerate-api.com/v6/ed51a583746de99a265453f3/latest/${currency}`;
+const APIKey = 'ed51a583746de99a265453f3';
+const getUrl = currency => 
+  `https://v6.exchangerate-api.com/v6/${APIKey}/latest/${currency}`;
 
 const getErrorMessage = errorType => ({
   'unsupported-code': 'A moeda não existe em nosso banco de dados',
@@ -93,53 +99,76 @@ const fetchExchangeRate = async url => {
     const exchangeRateData = await response.json();
 
     if (exchangeRateData.result === 'error') {
-      throw new Error(getErrorMessage(exchangeRateData['error-type']));
+      const errorMessage = getErrorMessage(exchangeRateData['error-type']);
+      throw new Error(errorMessage);
     }
 
-    return exchangeRateData;
+    return state.setExchangeRate(exchangeRateData);
   } catch(err) {
     showAlert(err);
   }
 }
+const getOptions = (selectedCurrency, conversion_rates) => {
+  const setSelectedAttribute = currency => 
+    currency === selectedCurrency ? 'selected': '';
+  const getOptionsAsArray = currency => 
+    `<option ${setSelectedAttribute(currency)}>${currency}</option>`
 
-const showInitialInfo = exchangeRate => {
-  const getOptions = selectedCurrency => Object.keys(exchangeRate.conversion_rates)
-  .map(currency => `<option ${currency === selectedCurrency ? 'selected': ''}>${currency}</option>`)
-  .join('');
+  return Object.keys(conversion_rates)
+    .map(getOptionsAsArray)
+    .join('');
+}
 
-  currencyOneEl.innerHTML = getOptions('USD');
-  currencyTwoEl.innerHTML = getOptions('BRL');
+const getMultipliedExchangeRate = conversion_rates => {
+  const currencyTwo = conversion_rates[currencyTwoEl.value];
+  return (timesCurrencyOneEl.value * currencyTwo).toFixed(2);
+}
 
-  convertedValueEl.innerHTML = exchangeRate.conversion_rates.BRL.toFixed(2);
-  valuePrecisionEl.innerHTML = `1 USD = ${exchangeRate.conversion_rates.BRL} BRL`;
+const getNotRoundedExchangeRate = conversion_rates => {
+  const currencyTwo = conversion_rates[currencyTwoEl.value];
+  return `1 ${currencyOneEl.value} = ${1 * currencyTwo} ${currencyTwoEl.value}`;
+}
+
+const showUpdatedRates = ({ conversion_rates }) => {
+  convertedValueEl.textContent = getMultipliedExchangeRate(conversion_rates);
+  valuePrecisionEl.textContent = getNotRoundedExchangeRate(conversion_rates);
+}
+
+const showInitialInfo = ({ conversion_rates }) => {
+  currencyOneEl.innerHTML = getOptions('USD', conversion_rates);
+  currencyTwoEl.innerHTML = getOptions('BRL', conversion_rates);
+
+  showUpdatedRates({ conversion_rates });
 }
 
 const init = async () => {
-  const exchangeRate = state.setExchangeRate(await fetchExchangeRate(getUrl('USD')));
+  const url = getUrl('USD');
+  const exchangeRate = await fetchExchangeRate(url);
 
   if (exchangeRate && exchangeRate.conversion_rates) {
     showInitialInfo(exchangeRate);
   }
 }
 
-const showUpdatedRates = exchangeRate => {
-  convertedValueEl.textContent = (timesCurrencyOneEl.value * exchangeRate.conversion_rates[currencyTwoEl.value]).toFixed(2);
-  valuePrecisionEl.textContent = `1 ${currencyOneEl.value} = ${1 * exchangeRate.conversion_rates[currencyTwoEl.value]} ${currencyTwoEl.value}`;
+const handleTimeCurrencyOneElInput = () => {
+  const { conversion_rates } = state.getExchangeRate();
+  convertedValueEl.textContent = getMultipliedExchangeRate(conversion_rates);
 }
 
-timesCurrencyOneEl.addEventListener('input', e => {
-  const exchangeRate = state.getExchangeRate();
-  convertedValueEl.textContent = (e.target.value * exchangeRate.conversion_rates[currencyTwoEl.value]).toFixed(2);
-});
-
-currencyTwoEl.addEventListener('input', () => {
+const handleCurrencyTwoElInput = () => {
   const exchangeRate = state.getExchangeRate();
   showUpdatedRates(exchangeRate);
-});
+}
 
-currencyOneEl.addEventListener('input', async e => {
-  const exchangeRate = state.setExchangeRate(await fetchExchangeRate(getUrl(e.target.value)));
+const handleCurrencyOneElInput = async e => {
+  const url = getUrl(e.target.value);
+  const exchangeRate = await fetchExchangeRate(url);
+
   showUpdatedRates(exchangeRate);
-});
+}
+
+timesCurrencyOneEl.addEventListener('input', handleTimeCurrencyOneElInput);
+currencyTwoEl.addEventListener('input', handleCurrencyTwoElInput);
+currencyOneEl.addEventListener('input', handleCurrencyOneElInput);
 
 init();
